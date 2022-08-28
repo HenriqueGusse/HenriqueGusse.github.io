4c47f399df60c4f5144862efff076c5cb509c77a
Henrique I Gusse <112174051+HenriqueGusse@users.noreply.github.com>
2022-08-27 23:14:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let buttonNo = document.querySelector('#buttonNo');
+buttonNo.onmouseover = trocarLugar;
+buttonNo.onclick = trocarLugar;
+
+let buttonYes = document.querySelector('#buttonYes');
+buttonYes.onclick = alegrarSe;
+
+function trocarLugar(){
+    buttonNo.style.left = Math.floor(Math.random() * 75) + '%';
+    buttonNo.style.top = Math.floor(Math.random() * 90) + '%';
+    console.log(buttonNo.style.left);
+    console.log(buttonNo.style.top);
+}
+
+function alegrarSe(){
+    alert('Te Amo! ❤️')
+}
