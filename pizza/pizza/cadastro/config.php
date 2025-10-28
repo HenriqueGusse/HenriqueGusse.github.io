85b4ab5e46504e9c59491806b215bff5dc4cb0f3
Henrique I Gusse <112174051+HenriqueGusse@users.noreply.github.com>
2025-10-28 20:29:35 -0300

    Delete pizza directory

--- pizza/pizza/cadastro/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Informações de conexão com o banco de dados
-define('DB_HOST', 'localhost'); // Endereço do servidor do banco de dados
-define('DB_USER', 'root');      // Nome do usuário do banco de dados
-define('DB_PASSWORD', '');      // Senha do usuário do banco de dados
-define('DB_NAME', 'cadastro'); // Nome do banco de dados
--- pizza/pizza/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // Desfaz todas as variáveis de sessão
-session_destroy(); // Destrói a sessão
-
-// Redireciona para a página de login após o logout
-header("Location: ../home/index.php");
-exit();
-?>
